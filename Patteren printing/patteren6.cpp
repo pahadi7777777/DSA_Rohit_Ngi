d74fb562d7fecd5c3050107431dd180971e12912
pahadi7777777 <amanchauhangx@gmail.com>
2026-05-13 01:33:42 +0530

    Add files via upload

---
+++ Patteren printing/patteren6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col,count=1;
+    for(row=1;row<=5;row++)
+    {
+        for(col=1;col<=5;col++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    
+    }
+}
