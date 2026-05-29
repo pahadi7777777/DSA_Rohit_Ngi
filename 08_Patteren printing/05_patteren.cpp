88702acc28179f670665df86a044502f98846e67
Aman Chauhan <amanchauhangx@gmail.com>
2026-05-29 22:13:53 +0530

    Add files via upload

---
+++ 08_Patteren printing/05_patteren.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,row,col;
+    cout<<"Enter the number: ";
+    cin>>n;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n-row;col++)// Space print
+        {
+            cout<<"  " ;
+        }
+        for(col=1;col<=row;col++)// Character print
+        {
+            cout<< row+1-col<<" " ;
+        }
+        cout<<endl;
+    }
+}
